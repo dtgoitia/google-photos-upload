e3d66d311803ef9637eeb9fbb9611cbaa81b7db8
David Torralba Goitia <david.torralba.goitia@gmail.com>
2019-02-03 13:02:06 +0000

    feat: add support for one file name type more

--- gpy/filenames.py
+++ gpy/filenames.py
@@ -15,6 +15,9 @@ def parse(file_name: str) -> Optional[datetime.datetime]:
     case_3 = parse_case_3(file_name)
     if case_3:
         return case_3
+    case_4 = parse_case_4(file_name)
+    if case_4:
+        return case_4
 
     return None
 
@@ -40,6 +43,24 @@ def parse_case_1(file_name: str) -> Optional[datetime.datetime]:
 def parse_case_2(file_name: str) -> Optional[datetime.datetime]:
     """Return timestamp from file name.
 
+    VID_YYYYMMDD_hhmmss_XXX.jpg, where XXX is a counter
+    """
+    pattern = r'VID_([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})_[0-9]{3}.mp4'
+    matches = re.match(pattern, file_name)
+    if matches is None:
+        return None
+    year = int(matches.group(1))
+    month = int(matches.group(2))
+    day = int(matches.group(3))
+    h = int(matches.group(4))
+    m = int(matches.group(5))
+    s = int(matches.group(6))
+    return datetime.datetime(year, month, day, h, m, s)
+
+
+def parse_case_3(file_name: str) -> Optional[datetime.datetime]:
+    """Return timestamp from file name.
+
     IMG-YYYYMMDD-WAXXXX.jpeg, where XXXX is a counter
     """
     pattern = r'IMG-([0-9]{4})([0-9]{2})([0-9]{2})-WA[0-9]{4}.jpeg'
@@ -52,7 +73,7 @@ def parse_case_2(file_name: str) -> Optional[datetime.datetime]:
     return datetime.datetime(year, month, day)
 
 
-def parse_case_3(file_name: str) -> Optional[datetime.datetime]:
+def parse_case_4(file_name: str) -> Optional[datetime.datetime]:
     """Return timestamp from file name.
 
     VID-YYYYMMDD-WAXXXX.mp4, where XXXX is a counter
--- tests/test_filenames.py
+++ tests/test_filenames.py
@@ -5,6 +5,7 @@ from gpy.filenames import parse
 
 @pytest.mark.parametrize(('file_name', 'expected_result'), [
     ('IMG_20190101_085024_211.jpg', datetime.datetime(2019, 1, 1, 8, 50, 24)),
+    ('VID_20190101_085024_211.mp4', datetime.datetime(2019, 1, 1, 8, 50, 24)),
     ('IMG-20190101-WA0001.jpeg', datetime.datetime(2019, 1, 1)),
     ('VID-20190101-WA0001.mp4', datetime.datetime(2019, 1, 1)),
     ('blah', None)
